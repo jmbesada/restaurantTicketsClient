3da4f84c11f7dd86d5b81ecee0bc60eeaa45e32a
jmbesada <jmbesada@dev.alphatool.es>
2012-08-06 18:49:33 +0200

    566556

--- src/com/josemi/restaurantticketsclient/MainActivity.java
+++ src/com/josemi/restaurantticketsclient/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html"); 
+        super.loadUrl("file:///android_asset/www/index.html");  
     }
 
   
